34a66022231bc4d8ef1cbabdfb8f82cf3f40aea7
Lance Groh-Prout <llance.grohprout@gmail.com>
2015-09-17 16:33:34 -0700

    rest of the assignment

---
+++ script.js
@@ -0,0 +1,166 @@
+//step 1
+
+
+//var someMonth;
+//function theMonth 		
+//currentMonth; 		
+//var summerMonth; 		
+//myLibraryAFunction; 	
+
+//step 2
+
+//numeric literal expression                   1 
+//a string literal expression                 "Lance"
+//a Boolean literal expression                "true"
+//null literal expression.                    "null"
+
+
+//step 3
+
+//var firstExpression = 1 * (2 / 3) + 200;
+//var secondExpression = " I love cake" + " and ice cream!"
+
+
+//step 4
+//
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var strZipCode;
+//var strMyAge;
+//var strReferralSource;
+//var strMayWeContact;
+
+
+//step 5
+//
+//var strFirstName;
+//    strFirstName = "Lance";
+//    
+//var strLastName = "Groh-Prout";
+//
+//var strAddress = "5555 Fake st.", strCity = "San Diego", strState = "California", strZipCode = "55555", strMyAge = "28", strReferralSource = "web", strMayWeContact = "yes";
+
+//step 6 
+//
+//var practice1 = 2 + 3; 
+//console.log(practice1);
+//
+//var practice2 = Boolean(10 > 9) + " That Lance is awesome!!"; 
+//console.log(practice2);
+//
+//var practice3 = 5 + Boolean( 9 > 7);
+//console.log(practice3);
+
+//step 7 
+
+//x = 1 / 0; 
+//
+//var x; 
+//console.log(xy);
+
+//step 8
+//
+//no it needs to be fixed as follows
+//
+//var someString = 'Who once said, "Only two thing are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+
+//step 9 
+//
+//var x = null;
+//window.console.log(x);
+//
+//var y = undefined;
+//window.console.log(y);
+
+//step 10 
+
+//
+//window.console.log(typeof " hi");
+//window.console.log(typeof 37);
+//window.console.log(typeof Boolean(3 > 2));
+//window.console.log(typeof undefined);
+//window.console.log(typeof {a: 1});
+//window.console.log(typeof 37);
+
+//step 11 
+//
+//window.console.log(window.alert(" Hello" + " Lance Groh-Prout" + ", welcome to the JavaScript class!"));
+
+//step 12 
+//var name = "Lance Groh-Prout";
+//window.console.log(window.alert("Hello " + name + ", welcome to the JavaScript class!"));
+
+//step 13 
+//var name = "Lance Groh-Prout";
+//var course = "JavaScript"; 
+//window.console.log(window.alert("Hello " + name + ", welcome to the " + course +  " class!"));
+
+//step 14 
+//var name = "Lance Groh-Prout";
+//var course = "JavaScript"; 
+//window.console.log(window.alert("Hello " + name + ",\n" + "Welcome to the " + course +  " class!"));
+
+//step 15
+//var name = prompt("What is your name?");
+//var course = "JavaScript"; 
+//window.console.log(window.alert("Hello " + name + ",\n" + "Welcome to the " + course +  " class!"));
+
+//step 16 
+
+//var name = prompt("What is your name?");
+//var course = prompt("What course are you taking?"); 
+//window.console.log(window.alert("Hello " + name + ",\n" + "Welcome to the " + course +  " class!"));
+
+//step 17
+//
+//var x = 10;
+//var y = 20;
+//window.console.log(x + y);
+
+//step 18
+
+//var x = 20;
+//var x = x + 20;
+//window.console.log(x);
+
+//step 19 
+
+//var x = 20;
+//var x = 5 * 20;
+//window.console.log(x);
+
+//step 20
+
+//var x = 20 % 3;
+//var x = x / 1;
+//window.console.log(x);
+
+//step 21 
+//
+//var x = 3 + 1 && 2 + 2 === 4;
+//window.console.log(x);
+
+//step 22 
+
+//var y = 2 + 2 | 1 + 3 == 10 
+//window.console.log(y < 3);
+
+//step 23 
+//
+//function Widget() {
+//}
+//var myWidget = new Widget();
+//window.console.log(typeof(Widget));
+
+//step 24 
+
+//var x = new String("black");
+//window.console.log(x instanceof String);
+
+//step 25 
+//
+//var x = new String("black");
+//window.console.log(x instanceof Number);
